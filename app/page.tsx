cdcd3e3a9f3d4b307f7ef9e84db60cb769d026bf
edwinkim16 <edwinkimathi226@gmail.com>
2025-12-10 19:49:49 +0300

    Setting up pages to be shown if user is logged in/ when not logged in

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
 import Image from "next/image";
+import { currentUser } from "@clerk/nextjs/server"
+import { redirect } from "next/navigation"
+import LandingPage from "@/components/LandingPage"
 
-export default function Home() {
-  return (
-    <div>
-      <h1>Calendar</h1>
-    </div>
+export default async function Home() {
+  const user = await currentUser()
+  // If no user is logged in, render the public landing page
+  if (!user) return <LandingPage />
+
+  // If user is logged in, redirect them to the events page
+  return redirect (
+    '/events'
   );
 }
